8cf2c7f25e2fcfe47dc904efde7f4550221d01b3
agzuniverse <aswinganesh666@gmail.com>
2020-11-18 20:39:52 +0530

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+import os
+
+from fabric.api import *
+
+PROJECT_BASE = "/home/excel/excelplay-backend-service/"
+
+def deploy():
+    print("here")
+    with cd(PROJECT_BASE):
+        print("inside context manager")
+        # run("docker-compose down")
+        # run("git pull origin master --rebase")
+        # run("docker-compose -f prod.docker-compose.yml up --build")
