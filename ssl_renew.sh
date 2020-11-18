9fb5208c806de45c5ca9e48ab40b8d089e037c68
agzuniverse <aswinganesh666@gmail.com>
2020-11-18 19:47:56 +0530

    Update ssl renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 DC="/usr/bin/docker-compose --no-ansi"
 DKR="/usr/bin/docker"
 
-cd /home/excel/excelplay-backend-kryptos/
-$DC run certbot renew && $DC kill -s SIGHUP excelplay-backend-nginx
+cd /home/excel/excelplay-backend-service/
+$DC -f prod.docker-compose.yml run certbot renew && $DC kill -s SIGHUP excelplay-backend-nginx
 $DKR system prune -af    
